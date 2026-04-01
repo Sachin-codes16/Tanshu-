2f028f2b2a38510481bc8a9634dfbf415b3bb9c2
Sachin-codes16 <sv4961941@gmail.com>
2026-04-01 22:14:16 +0530

    fix

--- src/components/ImageGallery/index.jsx
+++ src/components/ImageGallery/index.jsx
@@ -23,7 +23,7 @@ const ImageGallery = () => {
                     key={index}
                     style={{
                         width: '100%',
-                        height: '100vh',
+                        
                         overflow: 'hidden',
                     }}
                 >
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -63,7 +63,7 @@ const Hero = () => {
                 alt="hero"
                 style={{
                     width: '100%',
-                    height: '100%',
+                    
                     objectFit: 'cover',
                     objectPosition: 'center',
                     display: 'block',
