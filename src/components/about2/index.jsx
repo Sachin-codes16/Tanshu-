4fea65fa68e8b895ad165528c66d24dcedbb70cf
Sachin-codes16 <sv4961941@gmail.com>
2026-04-02 15:30:14 +0530

    romove space

--- src/components/about2/index.jsx
+++ src/components/about2/index.jsx
@@ -1,15 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import VideoModal from '../../components/ModalVideo'
-import abimg from '../../images/about2.jpg'
-import abimg2 from '../../images/about3.jpg'
-
-
-const About2 = (props) => {
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
-    
-}
+const About2 = () => {
+  return null;
+};
 
 export default About2;
